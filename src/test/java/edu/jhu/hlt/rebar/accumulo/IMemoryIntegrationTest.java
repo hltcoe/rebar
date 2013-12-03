aa135b16035c1ec9c278a3bae5f2ec742e3d5549
Max Thomas <maxjthomas@gmail.com>
2013-12-02 23:54:52 -0500

    add ITest for big integration

---
+++ src/test/java/edu/jhu/hlt/rebar/accumulo/IMemoryIntegrationTest.java
@@ -0,0 +1,25 @@
+package edu.jhu.hlt.rebar.accumulo;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IMemoryIntegrationTest extends AbstractAccumuloTest {
+
+  @Before
+  public void setUp() throws Exception {
+    this.initialize();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
