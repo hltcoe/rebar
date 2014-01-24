f657779b1332719886a542b26389092531979e70
Max Thomas <maxjthomas@gmail.com>
2014-01-24 01:41:19 -0500

    add miser to dev branch

---
+++ miser/src/main/thrift/ex.thrift
@@ -0,0 +1,8 @@
+namespace java edu.jhu.hlt.miser.java
+namespace py concrete.ex
+#@namespace scala edu.jhu.hlt.miser
+
+exception RebarException {
+  1: string message
+  2: optional binary serEx
+}
