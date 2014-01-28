503d040368aca31efe2e3b3727095c7a6b94c769
max thomas <max.thomas@jhuapl.edu>
2014-01-28 16:39:10 -0500

    Checkpoint before annotation-based annotation

--- scarbre/src/main/scala/edu/jhu/rebar/scarbre/accumulo/Annotation.scala
+++ scarbre/src/main/scala/edu/jhu/rebar/scarbre/accumulo/Annotation.scala
@@ -11,22 +11,35 @@ import edu.jhu.hlt.rebar._
 import com.twitter.scrooge.{ThriftStruct, ThriftStructCodec}
 import scala.util.{Try, Success, Failure}
 
-trait DocumentTableBacked extends TableBacked {
-  override val tableName = Configuration.DocumentTableName
-}
+// trait DocumentTableBacked extends TableBacked {
+//   override val tableName = Configuration.DocumentTableName
+// }
 
-sealed trait Annotation[T <: ThriftStruct] extends DocumentTableBacked {
+// sealed trait Annotation extends DocumentTableBacked {
+//   val anno[T <: ThriftStruct] : T
+//   def comm : Communication
+// }
+
+// case class SectionAnnotation (comm: Communication, anno: SectionSegmentation)
+//     extends Annotation[SectionSegmentation]
+
+// case class SentenceAnnotation(comm: Communication, anno: SentenceSegmentation)
+//     extends Annotation[SentenceSegmentation]
+
+// case class TokenizationAnnotation(comm: Communication, anno: Tokenization)
+//     extends Annotation[Tokenization]
+
+sealed trait Annotation[T <: ThriftStruct] {
   val anno : T
-  def comm : Communication
-  def toBytes[T <: ThriftStruct](x: ThriftStructCodec[T], y: T) : Array[Byte] = 
+  val comm : Communication
+
+  def toBytes[T <: ThriftStruct](x: ThriftStructCodec[T], y: T) : Array[Byte] =
     BinaryThriftStructSerializer(x).toBytes(y)
 }
+case class SectionAnnotation(anno: SectionSegmentation, comm: Communication) extends Annotation[SectionSegmentation]
 
-case class SectionAnnotation (comm: Communication, anno: SectionSegmentation) 
-    extends Annotation[SectionSegmentation]
+case class SentenceAnnotation(anno: SentenceSegmentation, comm: Communication) extends Annotation[SentenceSegmentation]
 
-case class SentenceAnnotation(comm: Communication, anno: SentenceSegmentation)
-    extends Annotation[SentenceSegmentation]
+case class TokenizationAnnotation(anno: Tokenization, comm: Communication) extends Annotation[Tokenization]
 
-case class TokenizationAnnotation(comm: Communication, anno: Tokenization)
-    extends Annotation[Tokenization]
+case class LangIdAnnotation(anno: LanguageIdentification, comm: Communication) extends Annotation[LanguageIdentification]
--- scarbre/src/main/scala/edu/jhu/rebar/scarbre/accumulo/Stage.scala
+++ scarbre/src/main/scala/edu/jhu/rebar/scarbre/accumulo/Stage.scala
@@ -17,86 +17,31 @@ trait StageTableBacked extends TableBacked {
 }
 
 /**
-  * A class that represents a basic Rebar "ingester", e.g., an
-  * interface to ingest `Communication` objects without annotations.
-  *
-  * @param conn The `Connector` object to use to connect to Accumulo.
+  * A useful trait wrapping around a `Stage`.
+  * Contains annotation methods. 
   */
+class RebarStage(stage: Stage) extends StageTableBacked {
+  val name = stage.name
 
-class RebarStage[+T <: ThriftStruct](s: Stage) 
-    extends StageTableBacked {
-
-  val name = s.name
-  if (!name.startsWith(stagePrefix))
-    throw new IllegalArgumentException(s"'$name' is not a valid stage name. Stage names must begin with '$stagePrefix'.")
-
-  if (!dependenciesExist)
-    throw new IllegalArgumentException(s"One or more dependencies for stage '$s.name' did not exist.")
-
-  def create : Try[IngestedStage[T]] = {
-    if (exists) Failure(new IllegalArgumentException(s"'$name already exists.'"))
+  def create : Try[Unit] = {
+    if (exists) 
+      Failure(new IllegalArgumentException(s"Stage '$name' already exists."))
     Try(createInternal)
   }
 
-  private def createInternal : IngestedStage[T] = {
+  protected def createInternal : Unit = {
     conn.withBatchWriter(Configuration.StagesTableName) { bw =>
       val m = new Mutation(name)
-      m.put(Configuration.StagesObjectCF, "", new Value(BinaryThriftStructSerializer(Stage).toBytes(s)))
+      m.put(Configuration.StagesObjectCF, "", new Value(BinaryThriftStructSerializer(Stage).toBytes(stage)))
       bw.addMutation(m)
     }
-
-    new IngestedStage[T](s)
   }
 
   def exists : Boolean = StageManager.exists(name)
+  // def addAnnotation (a: T, c: Communication) : Try[Unit] = {
+  //   Failure(new IllegalArgumentException("TODO"))
+  // }
 
-  private def dependenciesExist : Boolean = {
-    s.dependencies.map(StageManager.exists).forall(x => true)
-  }
-}
-
-/**
-  * A (more) immutable wrapper around a [[RebarStage]]. 
-  * Contains annotation methods. 
-  */
-class IngestedStage[+T <: ThriftStruct](s: Stage) extends StageTableBacked {
-  import scala.collection.JavaConverters._
-
-  val name = s.name
-  val dependencies = s.dependencies
-  val `type` = s.`type`
-  val description = s.description
-  val createTime = s.createTime
-
-  private def addAnnotatedDocumentId(comm: Communication) : Unit =
-    addAnnotatedDocumentId(comm.id)
-
-  private def addAnnotatedDocumentId(id: String) : Unit = {
-    conn.withBatchWriter(tableName) { bw =>
-      val m = new Mutation(name)
-      m.put(Configuration.StagesDocumentsAnnotationIdCF, id, AccumuloClient.EmptyValue)
-      bw.addMutation(m)
-    }
-  }
-
-  def getAnnotatedDocumentIds : Set[String] = {
-    val scan = conn.scanner(tableName).withRange(name)
-    scan.fetchColumnFamily(new Text(Configuration.StagesDocumentsAnnotationIdCF))
-    scan.iterator().asScala.map { e =>
-      e.getKey.getColumnQualifier.toString
-    }.toSet
-  }
-
-  def isAnnotated(c: Communication) : Boolean = {
-    conn.scanner(tableName).withRange(c.id).iterator().hasNext()
-  }
-
-  def annotate[K <: Annotation[T]](a: K) : Try[Unit] = {
-    if (isAnnotated(a.comm)) 
-      Failure(new IllegalArgumentException("TODO"))
-
-    Success()
-  }
 }
 
 object StageManager {
@@ -125,12 +70,73 @@ object StageManager {
   //     .toSet
   // }
 
-  def stageToRebarStage(s: Stage) : RebarStage[_ <: ThriftStruct] = {
-    s.`type` match {
-      case StageType.Section      => new RebarStage[SectionSegmentation](s)
-      case StageType.Sentence     => new RebarStage[SentenceSegmentation](s)
-      case StageType.Tokenization => new RebarStage[Tokenization](s)
-      case _ => throw new IllegalArgumentException("wtf")
-    }
-  }
 }
+
+// class RebarStage[+T <: ThriftStruct](s: Stage) 
+//     extends StageTableBacked {
+
+//   val name = s.name
+//   if (!name.startsWith(stagePrefix))
+//     throw new IllegalArgumentException(s"'$name' is not a valid stage name. Stage names must begin with '$stagePrefix'.")
+
+//   if (!dependenciesExist)
+//     throw new IllegalArgumentException(s"One or more dependencies for stage '$s.name' did not exist.")
+
+//   def create : Try[IngestedStage[T]] = {
+//     
+//     Try(createInternal)
+//   }
+
+//   private def createInternal : IngestedStage[T] = {
+
+
+//     new IngestedStage[T](s)
+//   }
+
+//   
+
+//   private def dependenciesExist : Boolean = {
+//     s.dependencies.map(StageManager.exists).forall(x => true)
+//   }
+// }
+
+// class IngestedStage[+T <: ThriftStruct](s: Stage) extends StageTableBacked {
+//   import scala.collection.JavaConverters._
+
+//   val name = s.name
+//   val dependencies = s.dependencies
+//   val `type` = s.`type`
+//   val description = s.description
+//   val createTime = s.createTime
+
+//   private def addAnnotatedDocumentId(comm: Communication) : Unit =
+//     addAnnotatedDocumentId(comm.id)
+
+//   private def addAnnotatedDocumentId(id: String) : Unit = {
+//     conn.withBatchWriter(tableName) { bw =>
+//       val m = new Mutation(name)
+//       m.put(Configuration.StagesDocumentsAnnotationIdCF, id, AccumuloClient.EmptyValue)
+//       bw.addMutation(m)
+//     }
+//   }
+
+//   def getAnnotatedDocumentIds : Set[String] = {
+//     val scan = conn.scanner(tableName).withRange(name)
+//     scan.fetchColumnFamily(new Text(Configuration.StagesDocumentsAnnotationIdCF))
+//     scan.iterator().asScala.map { e =>
+//       e.getKey.getColumnQualifier.toString
+//     }.toSet
+//   }
+
+//   def isAnnotated(c: Communication) : Boolean = {
+//     conn.scanner(tableName).withRange(c.id).iterator().hasNext()
+//   }
+
+//   def annotate[K <: Annotation[T]](a: K) : Try[Unit] = {
+//     if (isAnnotated(a.comm)) 
+//       Failure(new IllegalArgumentException("TODO"))
+
+//     Success()
+//   }
+// }
+
