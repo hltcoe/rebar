81e02b65216c7b9a441a2368a223852d6d93e6a8
max thomas <maxjthomas@gmail.com>
2014-03-14 21:34:03 -0400

    Update tift dependency.

--- ballast/src/main/java/edu/jhu/hlt/rebar/ballast/tools/TiftTokenizer.java
+++ ballast/src/main/java/edu/jhu/hlt/rebar/ballast/tools/TiftTokenizer.java
@@ -4,27 +4,48 @@
 package edu.jhu.hlt.rebar.ballast.tools;
 
 import edu.jhu.hlt.concrete.Communication;
+import edu.jhu.hlt.concrete.Section;
+import edu.jhu.hlt.concrete.Sentence;
+import edu.jhu.hlt.concrete.Tokenization;
 import edu.jhu.hlt.concrete.TokenizationCollection;
+import edu.jhu.hlt.concrete.util.SuperTextSpan;
 import edu.jhu.hlt.rebar.ballast.AnnotationException;
 import edu.jhu.hlt.rebar.ballast.AnnotationTool;
+import edu.jhu.hlt.tift.Tokenizer;
 
 /**
+ * A wrapper around Tift that provides {@link TokenizationCollection} objects
+ * for each {@link Sentence} in each {@link Section}.
+ * 
  * @author max
  *
  */
 public class TiftTokenizer implements AnnotationTool<TokenizationCollection> {
 
+  private final Tokenizer tokenizer;
+  
   /**
    * 
    */
-  public TiftTokenizer() {
-    // TODO Auto-generated constructor stub
+  public TiftTokenizer(Tokenizer tokenizer) {
+    this.tokenizer = tokenizer;
   }
 
   @Override
   public TokenizationCollection annotate(Communication c) throws AnnotationException {
-    // TODO Auto-generated method stub
-    return null;
+    Communication copy = new Communication(c);
+    TokenizationCollection tokColl = new TokenizationCollection();
+
+    for (Section s : copy.getSectionSegmentations().get(0).getSectionList()) {
+      for (Sentence st : s.getSentenceSegmentation().get(0).getSentenceList()) {
+        SuperTextSpan sts = new SuperTextSpan(st.getTextSpan(), copy);
+        String sentenceText = sts.getText();
+        Tokenization t = this.tokenizer.tokenizeToConcrete(sentenceText, 0);
+        tokColl.addToTokenizationList(t);
+      }
+    }
+    
+    return tokColl;
   }
 
 }
