4e90e99eb0bbbf8a7a3d0c3d4ab1aaa673f7ad79
max thomas <maxjthomas@gmail.com>
2014-03-24 13:51:10 -0400

    Updates to validation lib and test.

---
+++ ballast/src/main/java/edu/jhu/hlt/rebar/ballast/validation/ValidatableTextSpan.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package edu.jhu.hlt.rebar.ballast.validation;
+
+import edu.jhu.hlt.concrete.Communication;
+import edu.jhu.hlt.concrete.TextSpan;
+
+/**
+ * @author max
+ *
+ */
+public class ValidatableTextSpan extends AbstractAnnotation<TextSpan> {
+
+  public ValidatableTextSpan(TextSpan annotation) {
+    super(annotation);
+  }
+
+  @Override
+  public boolean isValid(Communication c) {
+    int begin = this.getAnnotation().start;
+    int end = this.getAnnotation().ending;
+    int textLength = c.getText().length();
+    
+    // Begin can't be negative
+    // End can't be equal or less than beginning
+    // End can't be > length of text
+    return begin >= 0 
+        && end > begin 
+        && end <= textLength;
+  }
+}
