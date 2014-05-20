27c4ef299457f5af36faf3025088e7102334e479
Max Thomas <max.thomas@jhu.edu>
2014-05-20 12:45:49 -0400

    Base MiniCluster abstract class.

---
+++ core/src/test/java/edu/jhu/hlt/rebar/accumulo/AbstractMiniClusterTest.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package edu.jhu.hlt.rebar.accumulo;
+
+import java.io.IOException;
+
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.Instance;
+import org.apache.accumulo.core.client.ZooKeeperInstance;
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
+import org.apache.accumulo.minicluster.MiniAccumuloCluster;
+import org.apache.accumulo.minicluster.MiniAccumuloConfig;
+
+import edu.jhu.hlt.rebar.RebarException;
+
+
+/**
+ * @author max
+ *
+ */
+public class AbstractMiniClusterTest extends AbstractAccumuloTest implements AutoCloseable {
+
+  protected final MiniAccumuloConfig cfg;
+  protected final MiniAccumuloCluster cluster;
+  
+  /**
+   * @param inst
+   * @throws RebarException 
+   */
+  public AbstractMiniClusterTest(MiniAccumuloConfig cfg) throws RebarException {
+    try {
+      this.cfg = cfg;
+      this.cluster = new MiniAccumuloCluster(this.cfg);
+      this.cluster.start();
+      Instance inst = new ZooKeeperInstance(this.cluster.getInstanceName(), this.cluster.getZooKeepers());
+      this.initialize(inst.getConnector("max", new PasswordToken(this.cfg.getRootPassword())));
+    } catch (IOException | InterruptedException | AccumuloException | AccumuloSecurityException e) {
+      throw new RebarException(e);
+    }
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.AutoCloseable#close()
+   */
+  @Override
+  public void close() throws Exception {
+    this.cluster.stop();
+  }
+}
